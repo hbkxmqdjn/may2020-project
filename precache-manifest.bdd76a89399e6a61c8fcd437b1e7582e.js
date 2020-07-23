52cdabc26554e0632d056adffc8d4785544f9271
Robynn Ding <hbkxmqdjn@gmail.com>
2020-07-23 19:47:34 -0400

    Updates

---
+++ precache-manifest.bdd76a89399e6a61c8fcd437b1e7582e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42bf8d6c93e9a647ffae502368a2f05c",
+    "url": "/may2020-project/index.html"
+  },
+  {
+    "revision": "e011157ecae570f31221",
+    "url": "/may2020-project/static/js/2.412ea8c8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/may2020-project/static/js/2.412ea8c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7edf0bbe96247be5632e",
+    "url": "/may2020-project/static/js/main.1d478a38.chunk.js"
+  },
+  {
+    "revision": "38f1509bba2ca2d3b15b",
+    "url": "/may2020-project/static/js/runtime-main.6f98421f.js"
+  }
+]);
